d93387967c1e52ae27bb08c520c2c0e2d9c88a0e
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-22 00:40:58 -0300

    10. Modo observador - Watch mode

--- bases/app.js
+++ bases/app.js
@@ -1,3 +1,8 @@
 "use strict";
-const msg = "Hola mundo";
-console.log(msg);
+const msg = "Hola mundo!!!";
+const hero = {
+    name: "Ironman",
+    age: 45,
+};
+hero.age = 50;
+console.log(hero.age + 1);
--- bases/app.ts
+++ bases/app.ts
@@ -1,3 +1,10 @@
-const msg: string = "Hola mundo";
+const msg: string = "Hola mundo!!!";
 
-console.log(msg);
+const hero = {
+   name: "Ironman",
+   age: 45,
+}
+
+hero.age = 50;
+
+console.log(hero.age + 1);
