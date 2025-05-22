861f5c1df6d482eebb566cc033f28b16270b627e
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-22 19:45:27 -0300

    14. Inferir tipos y modo estricto

--- bases/app.ts
+++ bases/app.ts
@@ -1,10 +1,4 @@
-const msg: string = "Hola mundo!!!";
-
-const hero = {
-   name: "Ironman",
-   age: 45,
-}
-
-hero.age = 50;
-
-console.log(hero.age + 1);
+(() => {
+   const a: number = 10;
+   console.log(a);
+})();
