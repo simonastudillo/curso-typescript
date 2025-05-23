fb7cd90c13341586f7020d8325ef830fa0c1aded
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-22 23:43:32 -0300

    22. Void - Vacío

---
+++ bases/tipos/void.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    function callBatman() {
+    }
+    const callSuperman = () => {
+    };
+    const a = callBatman();
+    console.log(a);
+    const b = callSuperman();
+    console.log(b);
+})();
---
+++ bases/tipos/void.ts
@@ -0,0 +1,14 @@
+(() => {
+
+   function callBatman(): void {
+
+   }
+
+   const callSuperman = (): void => {
+   };
+
+   const a = callBatman();
+   console.log(a);
+   const b = callSuperman();
+   console.log(b);
+})();
