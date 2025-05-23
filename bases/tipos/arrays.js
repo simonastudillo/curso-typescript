5777a7e463674d6f057833578579f04dd7f4aa2d
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-22 22:54:41 -0300

    19. Arrays - Arreglos

---
+++ bases/tipos/arrays.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    // const numbers: (string | number | boolean)[] = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // numbers.push(true);
+    numbers.push(11);
+    console.log(numbers);
+    const villians = ['Lex Luthor', 'Red Skull', 'Doom'];
+    villians.forEach(villian => {
+        console.log(villian.toUpperCase());
+    });
+})();
