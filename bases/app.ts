5b495e49463f776e1a699ab2c95203919a910da3
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-23 12:13:22 -0300

    34. Tarea y resolución del ejercicio práctico #2

--- bases/app.ts
+++ bases/app.ts
@@ -1,44 +1,27 @@
-(() => {
+// Funciones Básicas
+const sumar = (a: number, b: number): number => a + b;
 
-   // Tipos
-   const batman: string = 'Bruce';
-   const superman: string = 'Clark';
+const contar = (heroes: string[]): number => heroes.length;
 
-   const existe: boolean = false;
+const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
 
-   // Tuplas
-   const parejaHeroes: [string, string] = [batman, superman];
-   const villano: [string, number, boolean] = ['Lex Lutor', 5, true];
-
-   // Arreglos
-   const aliados: string[] = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
-
-   //Enumeraciones
-   enum Fuerza {
-      aquaman = 0,
-      batman = 1,
-      flash = 5,
-      superman = 100,
+//Parametros por defecto
+const llamarBatman = (llamar: boolean = false): void => {
+   if (llamar) {
+      console.log("Batiseñal activada");
    }
-   const fuerzaFlash: Fuerza = Fuerza.flash;
-   const fuerzaSuperman: Fuerza = Fuerza.superman;
-   const fuerzaBatman: Fuerza = Fuerza.batman;
-   const fuerzaAcuaman: Fuerza = Fuerza.aquaman;
+}
 
-   // Retorno de funciones
-   function activar_batiseñal(): string {
-      return 'activada';
-   }
+llamarBatman();
 
-   function pedir_ayuda(): void {
-      console.log('Auxilio!!!');
-   }
-
-   // Aserciones de Tipo
-   const poder: any = '100';
-   const largoDelPoder: number = (poder as string).length;
-   console.log(largoDelPoder);
+// Rest?
+const unirheroes = (...personas: string[]): string => personas.join(", ");
 
 
-})()
+// Tipo funcion
+const noHaceNada = (numero: number, texto: string, booleano: boolean, arreglo: string[]): void => { }
 
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco: (n: number, t: string, b: boolean, a: string[]) => void;
+noHaceNadaTampoco = noHaceNada
