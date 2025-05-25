373da21038c8dea2e9b2d25d09cc818040e732a7
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-24 23:26:02 -0300

    43. Tarea y Resolución del ejecicio práctico #3

--- bases/app.ts
+++ bases/app.ts
@@ -1,52 +1,80 @@
 
 // Objetos
-const batimovil = {
-  carroceria: "Negra",
-  modelo: "6x6",
-  antibalas: true,
-  pasajeros:4
+
+type heroCar = {
+   carroceria: string;
+   modelo: string;
+   antibalas: boolean;
+   pasajeros: number;
+   disparar?: () => void;
+}
+
+const batimovil: heroCar = {
+   carroceria: "Negra",
+   modelo: "6x6",
+   antibalas: true,
+   pasajeros: 4
 };
 
-const bumblebee = {
-  carroceria: "Amarillo con negro",
-  modelo: "4x2",
-  antibalas: true,
-  pasajeros:4,
-  disparar(){ // El metodo disparar es opcional
-    console.log("Disparando");
-  }
+const bumblebee: heroCar = {
+   carroceria: "Amarillo con negro",
+   modelo: "4x2",
+   antibalas: true,
+   pasajeros: 4,
+   disparar() { // El metodo disparar es opcional
+      console.log("Disparando");
+   }
 };
 
 
 // Villanos debe de ser un arreglo de objetos personalizados
-const villanos = [{
-  nombre:"Lex Luthor",
-  edad: 54,
-  mutante:false
-},{
-  nombre: "Erik Magnus Lehnsherr",
-  edad: 49,
-  mutante: true
-},{
-  nombre: "James Logan",
-  edad: undefined,
-  mutante: true
+
+type villian = {
+   nombre: string;
+   edad: number | undefined;
+   mutante: boolean;
+}
+
+
+const villanos: villian[] = [{
+   nombre: "Lex Luthor",
+   edad: 54,
+   mutante: false
+}, {
+   nombre: "Erik Magnus Lehnsherr",
+   edad: 49,
+   mutante: true
+}, {
+   nombre: "James Logan",
+   edad: undefined,
+   mutante: true
 }];
 
 // Multiples tipos
 // cree dos tipos, uno para charles y otro para apocalipsis
+
+type charlesType = {
+   poder: string;
+   estatura: number;
+}
+
 const charles = {
-  poder:"psiquico",
-  estatura: 1.78
+   poder: "psiquico",
+   estatura: 1.78
 };
 
+type apocalipsisType = {
+   lider: boolean;
+   miembros: string[];
+}
+
 const apocalipsis = {
-  lider:true,
-  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+   lider: true,
+   miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
 }
 
 // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
-let mystique;
+let mystique: (charlesType | apocalipsisType);
 
 mystique = charles;
 mystique = apocalipsis;
