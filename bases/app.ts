33ba7ca95b9d1bd155703adbdd72aedb90c10277
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-24 23:19:38 -0300

    42. Ejercicio práctico #3

--- bases/app.ts
+++ bases/app.ts
@@ -1,27 +1,52 @@
-// Funciones Básicas
-const sumar = (a: number, b: number): number => a + b;
 
-const contar = (heroes: string[]): number => heroes.length;
+// Objetos
+const batimovil = {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros:4
+};
 
-const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-contar(superHeroes);
+const bumblebee = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros:4,
+  disparar(){ // El metodo disparar es opcional
+    console.log("Disparando");
+  }
+};
 
-//Parametros por defecto
-const llamarBatman = (llamar: boolean = false): void => {
-   if (llamar) {
-      console.log("Batiseñal activada");
-   }
+
+// Villanos debe de ser un arreglo de objetos personalizados
+const villanos = [{
+  nombre:"Lex Luthor",
+  edad: 54,
+  mutante:false
+},{
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+},{
+  nombre: "James Logan",
+  edad: undefined,
+  mutante: true
+}];
+
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+const charles = {
+  poder:"psiquico",
+  estatura: 1.78
+};
+
+const apocalipsis = {
+  lider:true,
+  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
 }
 
-llamarBatman();
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique;
 
-// Rest?
-const unirheroes = (...personas: string[]): string => personas.join(", ");
-
-
-// Tipo funcion
-const noHaceNada = (numero: number, texto: string, booleano: boolean, arreglo: string[]): void => { }
-
-// Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco: (n: number, t: string, b: boolean, a: string[]) => void;
-noHaceNadaTampoco = noHaceNada
+mystique = charles;
+mystique = apocalipsis;
