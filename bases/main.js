0df77297b1dec6a309b797573a2c94f4fddd09a8
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-27 17:22:09 -0300

    61. Métodos públicos y privados

--- bases/main.js
+++ bases/main.js
@@ -1,13 +1,22 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.avgAge;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
+    console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
