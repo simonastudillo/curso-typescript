8dd6a84ef436e668183a557a4b13740f59dbcc31
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-28 17:40:28 -0300

    69. Estructuras complejas

---
+++ bases/interfaces/complex.ts
@@ -0,0 +1,35 @@
+(() => {
+
+   interface Address {
+      id: number;
+      zip: string;
+      city: string;
+   }
+
+   interface Client {
+      name: string;
+      age?: number;
+      address?: Address;
+   }
+
+   const client: Client = {
+      name: 'Complex Client',
+      age: 30,
+      address: {
+         id: 1,
+         zip: '12345',
+         city: 'New York'
+      }
+   }
+
+   const client2: Client = {
+      name: 'Melissa',
+      age: 25,
+      address: {
+         id: 2,
+         zip: '54321',
+         city: 'Los Angeles'
+      }
+   }
+
+})();
--- bases/main.js
+++ bases/main.js
@@ -13,4 +13,24 @@
     };
     console.log(superman);
 })();
+(() => {
+    const client = {
+        name: 'Complex Client',
+        age: 30,
+        address: {
+            id: 1,
+            zip: '12345',
+            city: 'New York'
+        }
+    };
+    const client2 = {
+        name: 'Melissa',
+        age: 25,
+        address: {
+            id: 2,
+            zip: '54321',
+            city: 'Los Angeles'
+        }
+    };
+})();
 //# sourceMappingURL=main.js.map
