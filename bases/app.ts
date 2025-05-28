68a591d798c36e37d4e3eaaea065ad7ae8b833e5
Simon Astudillo <simon.astudillo@raef.cl>
2025-05-28 20:59:05 -0300

    74. Tarea y Resolución del ejercicio #5

--- bases/app.ts
+++ bases/app.ts
@@ -1,40 +1,55 @@
 // Crear interfaces
-
-// Cree una interfaz para validar el auto (el valor enviado por parametro)
-const conducirBatimovil = ( auto ):void => {
-  auto.encender = true;
-  auto.velocidadMaxima = 100;
-  auto.acelear();
+interface Auto {
+   encender: boolean;
+   velocidadMaxima: number;
+   acelerar: () => void;
 }
 
-const batimovil = {
-  encender:false,
-  velocidadMaxima:0,
-  acelear(){
-    console.log("...... gogogo!!!");
-  }
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+const conducirBatimovil = (auto: Auto): void => {
+   auto.encender = true;
+   auto.velocidadMaxima = 100;
+   auto.acelerar();
+}
+
+const batimovil: Auto = {
+   encender: false,
+   velocidadMaxima: 0,
+   acelerar() {
+      console.log("...... gogogo!!!");
+   }
 }
 
 // Cree una interfaz con que permita utilzar el siguiente objeto
 // utilizando propiedades opcionales
 
-const guason = {
-  reir: true,
-  comer:true,
-  llorar:false
+interface Villano {
+   reir?: boolean;
+   comer?: boolean;
+   llorar?: boolean;
 }
 
-const reir = ( guason ):void => {
-  if( guason.reir ){
-    console.log("JAJAJAJA");
-  }
+const guason: Villano = {
+   reir: true,
+   comer: true,
+   llorar: false
+}
+
+const reir = (guason: Villano): void => {
+   if (guason.reir) {
+      console.log("JAJAJAJA");
+   }
 }
 
 
 // Cree una interfaz para la siguiente funcion
 
-const ciudadGotica = ( ciudadanos:string[] ):number => {
-  return ciudadanos.length;
+interface CiudadGoticaFn {
+   (ciudadanos: string[]): number;
+}
+
+const ciudadGotica: CiudadGoticaFn = (ciudadanos: string[]): number => {
+   return ciudadanos.length;
 }
 
 // Cree una interfaz que obligue crear una clase
@@ -48,4 +63,21 @@ const ciudadGotica = ( ciudadanos:string[] ):number => {
     - estadoCivil
     - imprimirBio(): void // en consola una breve descripcion.
 */
-class Persona {}
+
+interface PersonaInterface {
+   nombre: string;
+   edad: number;
+   sexo: ['masculino' | 'femenino'];
+   estadoCivil: ['soltero' | 'casado' | 'divorciado'];
+   imprimirBio: () => void
+}
+
+class Persona implements PersonaInterface {
+
+   public nombre: string;
+   public edad: number;
+   public sexo: ['masculino' | 'femenino'];
+   public estadoCivil: ['soltero' | 'casado' | 'divorciado'];
+
+   imprimirBio(): void { };
+}
