c022180d758b9cd14411acc109eaa437fcb3e253
Simon Astudillo <simon.astudillo@raef.cl>
2025-06-03 21:53:31 -0300

    87. Agrupar exportaciones

---
+++ ts-node/src/backs/generics.ts
@@ -0,0 +1,21 @@
+// import { Hero } from "./interfaces/hero";
+// import { genericArrowFunction } from "./generics/generics";
+// import { Villain } from './interfaces/villain';
+
+// printObject(123);
+// printObject(new Date());
+// printObject({ name: "Alice", age: 30 });
+// printObject("Hello, TypeScript!");
+// printObject([1, 2, 3, 4, 5]);
+
+// console.log(genericArrowFunction(3.1416).toFixed(2));
+// console.log(genericArrowFunction('Hola mundo').toUpperCase());
+// console.log(genericArrowFunction(new Date()).toDateString());
+
+// const deadpool = {
+//    name: "Deadpool",
+//    realName: "Wade Wilson",
+//    dangerLevel: 130
+// }
+
+// console.log(genericArrowFunction<Villain>(deadpool).dangerLevel);
