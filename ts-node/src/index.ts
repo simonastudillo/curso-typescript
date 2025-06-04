4f85b6e9f34f8de946ca462ac642d6f75f4220d5
Simon Astudillo <simon.astudillo@raef.cl>
2025-06-03 21:51:28 -0300

    86. Ejemplo de función genérica en acción

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,4 +1,6 @@
+import { Hero } from "./interfaces/hero";
 import { genericArrowFunction } from "./generics/generics";
+import { Villain } from './interfaces/villain';
 
 // printObject(123);
 // printObject(new Date());
@@ -6,6 +8,14 @@ import { genericArrowFunction } from "./generics/generics";
 // printObject("Hello, TypeScript!");
 // printObject([1, 2, 3, 4, 5]);
 
-console.log(genericArrowFunction(3.1416).toFixed(2));
-console.log(genericArrowFunction('Hola mundo').toUpperCase());
-console.log(genericArrowFunction(new Date()).toDateString());
+// console.log(genericArrowFunction(3.1416).toFixed(2));
+// console.log(genericArrowFunction('Hola mundo').toUpperCase());
+// console.log(genericArrowFunction(new Date()).toDateString());
+
+// const deadpool = {
+//    name: "Deadpool",
+//    realName: "Wade Wilson",
+//    dangerLevel: 130
+// }
+
+// console.log(genericArrowFunction<Villain>(deadpool).dangerLevel);
