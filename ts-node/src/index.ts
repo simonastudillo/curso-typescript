89fed7e1da8992e60ea7d3df84378e94260288b2
Simon Astudillo <simon.astudillo@raef.cl>
2025-06-05 21:27:04 -0300

    93. Introducción a los decoradores

--- ts-node/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { getPokemon } from "./generics/get-pokemon";
-
-getPokemon(1)
-   .then(pokemon => console.log(pokemon.name))
-   .catch(err => console.error(err))
-   .finally(() => console.log('Finished fetching Pokémon data'));
